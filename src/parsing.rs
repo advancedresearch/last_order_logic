941cf709834149e284ea166ea6e70d68d6cc638a
Sven Nilsen <bvssvni@gmail.com>
2021-11-01 16:10:42 +0100

    I

--- src/parsing.rs
+++ src/parsing.rs
@@ -25,6 +25,9 @@ pub fn parse_expr(
         } else if let Ok((range, _)) = convert.meta_bool("1") {
             convert.update(range);
             res = Some(_1);
+        } else if let Ok((range, _)) = convert.meta_bool("I") {
+            convert.update(range);
+            res = Some(I);
         } else {
             let range = convert.ignore();
             convert.update(range);
@@ -64,5 +67,6 @@ mod tests {
     fn expr() {
         assert_eq!(parse_str("0"), Ok(_0));
         assert_eq!(parse_str("1"), Ok(_1));
+        assert_eq!(parse_str("I"), Ok(I));
     }
 }
