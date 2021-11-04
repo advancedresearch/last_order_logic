e4a3bed1b8f8ce279f47ec4489987cf5253a55cf
Sven Nilsen <bvssvni@gmail.com>
2021-11-04 13:40:25 +0100

    Added "reload"
    Closes https://github.com/advancedresearch/last_order_logic/issues/61
    
    - Fixed "help"

--- src/runtime.rs
+++ src/runtime.rs
@@ -22,8 +22,13 @@ pub fn reduce(expr: &Expr, defs: &[(Arc<String>, Expr)]) -> Expr {
 }
 
 /// Handle data.
-pub fn data(x: &str, defs: &mut Vec<(Arc<String>, Expr)>, last_expr: &mut Option<Expr>) {
-    match parsing::parse_data_str(&x) {
+pub fn data(
+    data_txt: &str,
+    defs: &mut Vec<(Arc<String>, Expr)>,
+    last_expr: &mut Option<Expr>,
+    last_import: &mut Option<String>,
+) {
+    match parsing::parse_data_str(data_txt) {
         Ok(x) => {
             for d in x.into_iter() {
                 match d {
@@ -36,20 +41,21 @@ pub fn data(x: &str, defs: &mut Vec<(Arc<String>, Expr)>, last_expr: &mut Option
                         println!("LOL: Added `{}` to definitions", name);
                         defs.push((name, expr));
                     }
-                    parsing::Data::Import(file) => {
+                    parsing::Data::Import(file_name) => {
                         use std::fs::File;
                         use std::io::Read;
 
                         let mut s = String::new();
-                        let mut file = match File::open(&*file) {
+                        let mut file = match File::open(&*file_name) {
                             Ok(x) => x,
                             Err(err) => {
-                                eprintln!("Could not open `{}`, {}", file, err);
+                                eprintln!("Could not open `{}`, {}", file_name, err);
                                 return;
                             }
                         };
                         file.read_to_string(&mut s).unwrap();
-                        data(&s, defs, last_expr);
+                        *last_import = Some(data_txt.to_string());
+                        data(&s, defs, last_expr, last_import);
                     }
                 }
             }
